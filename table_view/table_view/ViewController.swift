251ddf66cf5ec554ad22226f35d7cc8fbb47f029
aminhp93 <minhpn.org.ec@gmail.com>
2016-11-08 20:09:53 -0800

    table view

---
+++ table_view/table_view/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  table_view
+//
+//  Created by Minh Pham on 11/8/16.
+//  Copyright © 2016 Minh Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        table.reloadData()
+    }
+    
+    @IBOutlet weak var table: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        let timesTable = Int(sliderValue.value * 20)
+        cell.textLabel?.text = String(timesTable * indexPath.row + 1)
+        return cell
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
